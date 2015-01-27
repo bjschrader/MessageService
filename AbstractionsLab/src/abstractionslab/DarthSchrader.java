6be2be96a73edf44bd7802843df613538888d567
Brian J Schrader <bschrader@my.wctc.edu>
2015-01-26 18:26:29 -0600

    Added imaginary

---
+++ AbstractionsLab/src/abstractionslab/DarthSchrader.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstractionslab;
+
+/**
+ *
+ * @author schra_000
+ */
+public class DarthSchrader implements SithLord {
+    
+}
